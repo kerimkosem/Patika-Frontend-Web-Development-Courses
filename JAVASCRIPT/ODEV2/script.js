d302072ad74b41346bc4e8fb6eddec328a4c264a
kerim <krmksmsite@gmail.com>
2022-03-29 13:02:28 +0300

    first commit

---
+++ JAVASCRIPT/ODEV2/script.js
@@ -0,0 +1,10 @@
+let buttonDOM = document.querySelector("#liveToastBtn")
+let listDOM = document.querySelector("#list")
+let taskDOM = document.querySelector("#task")
+let ulLength = document.getElementsByTagName("li")
+
+for (i = 0; i < ulLength.length; i++ ){
+
+    let closeButton = document.createElement("span")
+    
+}
